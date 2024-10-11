edebf8d6ea3ce463d12c53b979ac3244d248c29a
Kirill Kaimusov <kaimusovks@yandex.ru>
2024-10-11 21:59:32 +0500

    Решение задачи 1

---
+++ event-object/dropdown/task.js
@@ -0,0 +1,20 @@
+const dropdownList = document.querySelectorAll('.dropdown__list');
+const dropdownValue = document.querySelectorAll('.dropdown__value');
+
+console.log(Array.from(dropdownValue));
+
+
+Array.from(dropdownValue).forEach((item) => {
+    item.addEventListener('click', () => {
+        item.nextElementSibling.classList.toggle('dropdown__list_active');
+    })
+})
+
+Array.from(dropdownList).forEach((item) => {
+    item.addEventListener('click', (event) => {
+        event.preventDefault();
+        item.closest('.dropdown').querySelector('.dropdown__value').textContent = event.target.textContent;
+        item.classList.remove('dropdown__list_active');
+    })
+})
+
