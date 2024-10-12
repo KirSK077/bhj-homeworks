0ff7788b9b85e6e3a2b87da123104f57ab3863e8
Kirill Kaimusov <kaimusovks@yandex.ru>
2024-10-13 04:41:13 +0500

    Исправлениие задания 3

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -1,14 +1,17 @@
 const tabs = Array.from(document.querySelectorAll('.tab'));
 const tabContents = Array.from(document.querySelectorAll('.tab__content')); 
+let activeTabIndex = 0
 
-
-
-tabs.forEach((tab, index) => {
+tabs.forEach((tab) => {
     tab.addEventListener('click', () => {
-        tab.classList.toggle('tab_active');
+        activeTabIndex = tabs.indexOf(tab);
+        tabs.forEach((tab) => {
+            tab.classList.remove('tab_active');
+        })
+        tab.classList.add('tab_active');
         tabContents.forEach((tabContent) => {
             tabContent.classList.remove('tab__content_active');
-        });
-        tabContents[index].classList.add('tab__content_active');
-    });
+        })
+        tabContents[activeTabIndex].classList.add('tab__content_active');
+    })
 })
